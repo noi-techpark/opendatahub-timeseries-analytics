6cd8bb61f03d3c9d6d7f3f07b360c4f7831daeb3
davide <davide@j8>
2018-04-04 14:52:15 +0200

    leggere i layer da un file di configurazione. caricamento dei dati solo nel momento in cui viene selezionato il layer o quando si preme il pulsante reload (predisposto ma non implementato ancora l'aggiornamento periodico)

---
+++ src/main/webapp/prototipo-mappe/map2.js
@@ -0,0 +1,201 @@
+function fetchJson_promise(url)
+{
+   return new Promise(function(success, fail)
+   {
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("GET", url , true);
+        xhttp.onreadystatechange = function(readystatechange)
+        {
+           if (xhttp.readyState == 4) // DONE:
+                                        // https://developer.mozilla.org/it/docs/Web/API/XMLHttpRequest/readyState
+           {
+
+               if (xhttp.status == 200)
+               {
+                   var data = JSON.parse(xhttp.responseText)
+                   success(data)
+               }
+               else
+               {
+                   fail(xhttp.status)
+               }
+           }
+        }
+        xhttp.send();
+   })
+}
+
+
+async function map_start_promise()
+{
+   var layers_container = document.getElementById('layers-container')
+   var layer_template = document.getElementById('layer_template_id');
+   layer_template.removeAttribute('id')
+   layers_container.removeChild(layer_template)
+   
+   
+   let map = new ol.Map({
+         target : 'map',
+         layers : [ new ol.layer.Tile({
+            source : new ol.source.OSM()
+         })
+
+         ],
+         view : new ol.View({
+
+            enableRotation : false, // altrimenti sballano i tile calcolati da me
+            center : ol.proj.fromLonLat([ 11.34, 46.48 ]),
+            // zoom iniziale mappa
+            zoom : 12
+
+         })
+      });
+   
+
+   let json = await fetchJson_promise('layers-config.json')
+   console.log(json)
+   
+   for (var layer_info of json)
+   {
+      console.log(layer_info)
+      let layer_display = layer_template.cloneNode(true)
+      layer_display.querySelector('.label').textContent = layer_info.id
+      layer_display.querySelector('.icon').src = layer_info.icon
+      layers_container.appendChild(layer_display)
+      
+      // inizia caricamento dati (mostrare un progress?)
+      let format = layer_info.format
+      console.log(format)
+      
+      switch (format)
+      {
+         case 'integreen':
+            loadIntegreenLayer(map, layer_info, layer_display)
+            break;
+         default:
+            alert('Unknow format: ' + format)
+            break;
+      }
+      
+      
+   }
+   
+   async function loadIntegreenLayer(map, layer_info, layer_display)
+   {
+      var iconStyle = new ol.style.Style({
+         image: new ol.style.Icon({
+           anchor: [0.5, 1.0],
+           anchorXUnits: 'fraction',
+           anchorYUnits: 'fraction',
+           opacity: 1,
+           src: layer_info.icon,
+           scale: 0.5
+         })
+       });
+       
+       var shadowStyle = new ol.style.Style({
+          image: new ol.style.Icon({
+            anchor: [0.3, 1.0],
+            anchorXUnits: 'fraction',
+            anchorYUnits: 'fraction',
+            opacity: 1,
+            src: 'marker-shadow.png',
+            scale: 1
+          })
+        });
+      
+       
+      let layer = null;
+      let loading_in_progress = false;
+      
+      
+      let spinner = layer_display.querySelector('.spinner')
+      
+       
+      layer_display.querySelector('.label').addEventListener('click', async function()
+      {
+         
+         if (layer)
+         {
+            // rimuovi dalla lista di autoaggiornamenti!
+            layer_display.classList.remove('selected')
+            map.removeLayer(layer)
+            layer = null;
+         } 
+         else
+         {         
+            layer_display.classList.add('selected')
+            
+            var sourcevector = new ol.source.Vector({});
+   
+            layer = new ol.layer.Vector({
+              title : 'meteoLayer',
+              visible : true,
+              source : sourcevector,
+              style : [shadowStyle, iconStyle]
+            })
+            
+            map.addLayer(layer)
+            
+            await load_refresh_features()
+         }
+      })
+      
+      spinner.addEventListener('click', load_refresh_features);
+      
+      async function load_refresh_features()
+      {
+         // cliccare su reload se il layer non è visibile, salta questa parte!
+         if (layer == null)
+            return;
+         
+         spinner.classList.add('loading')
+         loading_in_progress = true;
+         
+         let json = await fetchJson_promise(layer_info.url)
+         console.log(json)
+         
+         var sourcevector = new ol.source.Vector({});
+
+         for (var i = 0; i < json.length; i++)
+         {
+            var thing = new ol.geom.Point(ol.proj.transform([ json[i].longitude,
+                  json[i].latitude ], layer_info.projection, 'EPSG:3857'));
+            var featurething = new ol.Feature({
+               // name: "Thing",
+               geometry : thing
+            });
+            
+            featurething.setProperties(json[i])
+
+            sourcevector.addFeature(featurething);
+         }
+         
+         layer.setSource(sourcevector)
+         
+         spinner.classList.remove('loading')
+         loading_in_progress = false;
+      }
+      
+      /*
+      var sourcevector = new ol.source.Vector({});
+
+      
+       var layer = new ol.layer.Vector({
+         title : 'meteoLayer',
+         visible : false,
+         source : sourcevector,
+         style : [shadowStyle, iconStyle]
+      })
+
+      map.addLayer(layer)
+      
+      
+      
+         layer.setVisible(true)
+      })
+
+
+      */
+   }
+}
