5b8f7f406505203455e5f924d190aea829b6ece9
Chris Mair <chris@1006.org>
2018-04-15 03:58:11 +0200

    work in progress by chris

--- src/main/webapp/prototipo-grafici/gfx.js
+++ src/main/webapp/prototipo-grafici/gfx.js
@@ -2,21 +2,24 @@
 
 let state = {
     active_tab: 0,
-    scale: { from:  1522010124044, 
-             to:    1522096524044,
-             range: 86400000
+    scale: {
+        from:  1522010124044, 
+        to:    1522096524044,
+        range: 86400000
            },
     graphs: [
-        { kind:             "MeteoFrontEnd",
+        { category:         "meteorology",
           station:          "23200MS",
-          station_human:    "Meran/Gratsch",
-          name:             "air-temperature",
+          station_name:     "Meran/Gratsch",
+          data_type:        "air-temperature",
+          unit:             "blabla",
           period:           "600"
         },
-        { kind:             "MeteoFrontEnd",
+        { category:         "meteorology",
           station:          "23200MS",
-          station_human:    "Meran/Gratsch",
-          name:             "precipitation",
+          station_name:     "Meran/Gratsch",
+          data_type:        "precipitation",
+          unit:             "blabla",
           period:           "300"
         }
 
@@ -31,9 +34,18 @@ const dump_state = () => {
 };
 
 
-const $  = document.querySelector.bind(document);
+const qs  = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
+const URL_PREFIX = "http://ipchannels.integreen-life.bz.it";
+const T0 = Number(new Date());
+
+const DEBUG = true;
+const debug_log = (msg) => {
+    if (DEBUG) {
+        console.log("gfx " + String(Number(new Date()) - T0) + ": " + msg);
+    }
+};
 
 const show_tab = ix => {
 
@@ -41,7 +53,7 @@ const show_tab = ix => {
     tabs.forEach( el => el.classList.remove("gfx_tab_active") );
     tabs[ix].classList.add("gfx_tab_active");
 
-    let panels = Array.from($$("#gfx_panels > div"));
+    let panels = Array.from($$("#gfx_tabbed_panels > div"));
     panels.forEach( el => el.style.display = "none" );
     panels[ix].style.display = "block";
 
@@ -107,8 +119,8 @@ let plot = (from, delta, reload)  => {
     state.scale.range = delta;
 
     if (reload) {
-        $("#gfx_flot").style.display = "none";
-        $("#gfx_wait").style.display = "block";
+        qs("#gfx_flot").style.display = "none";
+        qs("#gfx_wait").style.display = "block";
 
         // Meran/Gratsch air temperature
 
@@ -125,14 +137,14 @@ let plot = (from, delta, reload)  => {
         return;
     }
 
-    $("#gfx_wait").style.display = "none";
-    $("#gfx_flot").style.display = "block";
+    qs("#gfx_wait").style.display = "none";
+    qs("#gfx_flot").style.display = "block";
 
     let temp = tempdata.map( el => [ el.timestamp, Number(el.value) ] );
     let prec = precdata.map( el => [ el.timestamp, Number(el.value) ] ); 
 
     if (temp.length === 0 && prec.length === 0) {
-        $("#gfx_flot").style.display = "none";
+        qs("#gfx_flot").style.display = "none";
         return;
     }
 
@@ -200,3 +212,152 @@ let plot = (from, delta, reload)  => {
 plot(state.scale.from, state.scale.range, true);
 
 window.addEventListener("resize", () => plot(state.scale.from, state.scale.range, false) );
+
+
+
+// tabbed panels -> add data set
+
+qs("#gfx_selstation").style.display = "none";
+qs("#gfx_seldataset").style.display = "none";
+qs("#gfx_addset").style.display = "none";
+
+qs("#gfx_selcategory").addEventListener("change", (ev) => {
+
+    let cat = getSelectValue(ev.target);
+    debug_log("event: #gfx_selcategory change fired with cat = " + cat);
+
+    switch (cat) {
+
+        case "":
+
+            qs("#gfx_selstation").style.display = "none";
+            qs("#gfx_seldataset").style.display = "none";
+            qs("#gfx_addset").style.display = "none";
+            break;
+
+        case "meteorology":
+
+            jQuery.getJSON(URL_PREFIX + "/MeteoFrontEnd/rest/get-station-details", (data) => {
+                debug_log("ajax: got data length = " + data.length);
+                let opt = `<option value="">select station...</option>`;
+                opt += data
+                        .sort( (a, b) => a.name > b.name? 1: -1 )
+                        .map( station => `<option value="${station.id};${station.name};">&rarr; ${station.name}</option>` )
+                        .join("\n");
+                let next = qs("#gfx_selstation");
+                next.innerHTML = opt;
+                next.style.display = "inline-block";
+            });
+            break;
+
+    }
+ 
+});
+
+qs("#gfx_selstation").addEventListener("change", (ev) => {
+
+    let station = getSelectValue(ev.target);
+    station = (station.split(";"))[0];
+
+    debug_log("event: #gfx_selstation change fired with station = " + station);
+
+    switch (station) {
+
+        case "":
+
+            qs("#gfx_seldataset").style.display = "none";
+            qs("#gfx_addset").style.display = "none";
+            break;
+
+        default: 
+
+            jQuery.getJSON(URL_PREFIX + "/MeteoFrontEnd/rest/get-data-types?station=" + station, (data) => {
+                debug_log("ajax: got data length = " + data.length);
+                let opt = `<option value="">select dataset...</option>`;
+                opt += data
+                        .sort( (a, b) => a[0] > b[0]? 1: -1 )
+                        .map( type => `<option value="${type[0]};${type[1]};${type[3]}">&rarr; ${type[0]} [${type[1]}] (${type[3]}s)</option>` )
+                        .join("\n");
+                let next = qs("#gfx_seldataset");
+                next.innerHTML = opt;
+                next.style.display = "inline-block";
+            });
+
+            break;
+
+    }
+ 
+});
+
+qs("#gfx_seldataset").addEventListener("change", (ev) => {
+
+    let dataset = getSelectValue(ev.target);
+    debug_log("event: #gfx_seldataset change fired with dataset = " + dataset);
+
+    switch (dataset) {
+
+        case "":
+
+            qs("#gfx_addset").style.display = "none";
+            break;
+
+        default: 
+            let next = qs("#gfx_addset");
+            next.style.display = "inline-block";
+            break;
+
+    }
+ 
+});
+
+qs("#gfx_addset").addEventListener("click", (ev) => {
+
+    let category     = getSelectValue(qs("#gfx_selcategory"));
+    let station      = (getSelectValue(qs("#gfx_selstation")).split(";"))[0];
+    let station_name = (getSelectValue(qs("#gfx_selstation")).split(";"))[1];
+    let data_type    = (getSelectValue(qs("#gfx_seldataset")).split(";"))[0];
+    let unit         = (getSelectValue(qs("#gfx_seldataset")).split(";"))[1];
+    let period       = (getSelectValue(qs("#gfx_seldataset")).split(";"))[2];
+
+    let obj = { "category":     category,
+                "station":      station,
+                "station_name": station_name,
+                "data_type":    data_type,
+                "unit":         unit,
+                "period":       period
+              };
+
+    let found = false;
+
+    state.graphs.forEach( graph => {
+        if ( graph.category  === obj.category &&
+             graph.station   === obj.station &&
+             graph.data_type === obj.data_type &&
+             graph.period    === obj.period
+           ) {
+            found = true;
+        }
+    });
+
+    if (found) {
+        debug_log("nothing added, graph was already present: " + JSON.stringify(obj));
+    } else {
+        state.graphs.push(obj);
+        debug_log("added graph: " + JSON.stringify(obj));
+    }
+
+    qs("#gfx_selcategory").value = "";
+    qs("#gfx_selstation").style.display = "none";
+    qs("#gfx_seldataset").style.display = "none";
+    qs("#gfx_addset").style.display = "none";
+    show_tab(0);
+
+});
+
+
+const getSelectValue = (element) => {
+    if (element === null || element === undefined) {
+        return undefined;
+    }
+    return element.options[element.selectedIndex].value;
+};
