7b7d2167271c74404224fee71b5b762fed65fd6b
davide <davide@j8>
2018-04-15 17:32:06 +0200

    riorganizzazione del codice

--- src/main/webapp/prototipo-mappe/map2.js
+++ src/main/webapp/prototipo-mappe/map2.js
@@ -70,42 +70,161 @@ async function map_start_promise()
       let format = layer_info.format
       console.log(format)
       
+      configureLayer(map, layer_info, layer_display)
+      
+      /*
       switch (format)
       {
          case 'integreen':
-            loadIntegreenLayer(map, layer_info, layer_display)
+            // loadIntegreenLayer(map, layer_info, layer_display)
             break;
          case 'wms':
-            loadWMSLayer(map, layer_info, layer_display)
+            // loadWMSLayer(map, layer_info, layer_display)
             break;
          default:
             alert('Unknow format: ' + format)
             break;
       }
-      
+      */
       
    }
-
-   async function loadWMSLayer(map, layer_info, layer_display)
+   
+   function configureLayer(map, layer_info, layer_display)
    {
-      var sourcetile = new ol.source.TileWMS({
-         url: layer_info.url ,
-         serverType: 'geoserver'
-       })
+      let spinner = layer_display.querySelector('.spinner')
       
-      sourcetile.on('tileloadend', function(event) {
-         console.log('immagini wms caricate!') 
+      let layer = null;
+      
+      let loading = false;
+      
+      layer_display.addEventListener('click', async function()
+      {
+         if (loading)
+            return;
+         
+         if (layer)
+         {
+            // rimuovi dalla lista di autoaggiornamenti!
+            layer_display.classList.remove('selected')
+            map.removeLayer(layer)
+            layer = null;
+         } 
+         else
+         {         
+            layer_display.classList.add('selected')
+            spinner.classList.add('loading')
+            loading = true
+            layer = await loadLayer_promise(map, layer_info)
+            map.addLayer(layer)
+            spinner.classList.remove('loading')
+            loading = false
+         }
       })
-      
-      sourcetile.on('tileloaderror', function(event) {
-         console.log('immagini wms caricate!') 
+   }
+   
+   async function loadLayer_promise(map, layer_info)
+   {
+      return new Promise(async function(ok, fail)
+      {
+         let format = layer_info.format
+         let layer = null;
+         switch (format)
+         {
+            case 'integreen':
+               // loadIntegreenLayer(map, layer_info, layer_display)
+               layer = await loadIntegreenLayer2(layer_info)
+               ok(layer)
+               break;
+            case 'wms':
+               layer = await loadWMSLayer(layer_info)
+               ok(layer)
+               break;
+            default:
+               alert('Unknow format: ' + format)
+               break;
+         }
+         // setTimeout(function(){ ok(1) }, 1000)
       })
-      
-      var layer = new ol.layer.Tile({
-         source: sourcetile
-       })
+   }
+   
+   async function loadIntegreenLayer2(layer_info)
+   {
+      return new Promise(async function(ok,fail)
+      {
+         var iconStyle = new ol.style.Style({
+            image: new ol.style.Icon({
+              anchor: [0.5, 1.0],
+              anchorXUnits: 'fraction',
+              anchorYUnits: 'fraction',
+              opacity: 1,
+              src: layer_info.icon,
+              scale: 0.5
+            })
+          });
+          
+          var shadowStyle = new ol.style.Style({
+             image: new ol.style.Icon({
+               anchor: [0.3, 1.0],
+               anchorXUnits: 'fraction',
+               anchorYUnits: 'fraction',
+               opacity: 1,
+               src: 'marker-shadow.png',
+               scale: 1
+             })
+           });
+          
+          var sourcevector = new ol.source.Vector({});
+          
+          let layer = new ol.layer.Vector({
+            title : 'meteoLayer',
+            visible : true,
+            source : sourcevector,
+            style : [shadowStyle, iconStyle]
+          })
+          
+          let json = await fetchJson_promise(layer_info.url)
+          
+          for (var i = 0; i < json.length; i++)
+          {
+             var thing = new ol.geom.Point(ol.proj.transform([ json[i].longitude,
+                   json[i].latitude ], layer_info.projection, 'EPSG:3857'));
+             var featurething = new ol.Feature({
+                // name: "Thing",
+                geometry : thing
+             });
+             
+             featurething.setProperties(json[i])
 
-      map.addLayer(layer)
+             sourcevector.addFeature(featurething);
+          }
+          
+          layer.setSource(sourcevector)
+          
+          ok(layer)
+      })
+       
+   }
+
+   async function loadWMSLayer(layer_info)
+   {
+      return new Promise(function (ok, fail)
+      {
+         var sourcetile = new ol.source.TileWMS({
+            url: layer_info.url ,
+            serverType: 'geoserver'
+          })
+         
+         sourcetile.on('tileloadstart', function(event) {
+            console.log('immagini wms caricate!') 
+         })
+         
+         var layer = new ol.layer.Tile({
+            source: sourcetile
+         })
+         
+         ok(layer)
+
+      })
    }
 
    async function loadIntegreenLayer(map, layer_info, layer_display)
