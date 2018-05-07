d4f4c5997811b752ae89320d2b53256f0ded7e49
davide <davide@j8>
2018-05-07 17:56:58 +0200

    aggiunta la query string

--- src/main/java/com/idmsuedtirol/geobank/analytics/GeobankAnalyticsServlet.java
+++ src/main/java/com/idmsuedtirol/geobank/analytics/GeobankAnalyticsServlet.java
@@ -66,6 +66,7 @@ public class GeobankAnalyticsServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
 		String requestPath = req.getPathInfo();
+		String urlParameters = req.getQueryString();
 		Matcher serviceMatcher = firstSubfolder.matcher(requestPath);
 		// If url is not in an expected format, return 404
 		if (!serviceMatcher.matches())
@@ -91,13 +92,12 @@ public class GeobankAnalyticsServlet extends HttpServlet {
 		// TODO: add some headers about browser caching?
 		
 		// TODO: match servicePath against a whitelist
-		String newUrlTxt = serviceUrlTxt + servicePath;
+		String newUrlTxt = serviceUrlTxt + servicePath + "?" + urlParameters;
 		System.out.println(newUrlTxt);
 		
 		URL serviceUrl = new URL(newUrlTxt);
 		HttpURLConnection conn = (HttpURLConnection) serviceUrl.openConnection();
 		
-		// TODO: try caarg0tch timeout exceptions, then return 504
 		conn.setConnectTimeout(15000);
 		conn.setReadTimeout(30000);
 		
