f0eb18fc75f2a9345b77443dd790d53421b3e93b
thomas <t.nocker.tn@gmail.com>
2020-05-26 16:56:23 +0200

    Keycloak authetification added

--- src/main/webapp/config.js
+++ src/main/webapp/config.js
@@ -1,2 +1,7 @@
 var thunderforest_api_key = ''
 var open_mobility_api_uri = 'https://mobility.api.opendatahub.bz.it'
+
+var KEYCLOAK_AUTHORIZATION_URI = ''
+var KEYCLOAK_REALM = ''
+var KEYCLOAK_CLIENT_ID = ''
+var KEYCLOAK_REDIRECT_URI = ''
