618db05d7e84c75e3516e11f8b9d8d9870593eed
Peter Moser <p.moser@noi.bz.it>
2020-06-04 17:53:40 +0200

    CI: Fix some typos

--- infrastructure/Jenkinsfile-CI.groovy
+++ infrastructure/Jenkinsfile-CI.groovy
@@ -1,7 +1,7 @@
 pipeline {
     agent {
         dockerfile {
-            filename 'docker/Dockerfile'
+            filename 'infrastructure/docker/Dockerfile'
             additionalBuildArgs  '--build-arg JENKINS_USER_ID=`id -u jenkins` --build-arg JENKINS_GROUP_ID=`id -g jenkins`'
         }
     }
@@ -9,7 +9,7 @@ pipeline {
     stages {
         stage('Test') {
             steps {
-                sh 'mvn -B -U clean test'
+                sh 'mvn -B -U clean compile test'
             }
         }
     }
