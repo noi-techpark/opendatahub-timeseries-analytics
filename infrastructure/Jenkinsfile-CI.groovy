7f8e6aeef4f72a5b41610aa80a454421fc6b1194
Peter Moser <p.moser@noi.bz.it>
2020-06-04 17:35:42 +0200

    Introduce the new infrastructure folder

--- infrastructure/Jenkinsfile-CI.groovy
+++ infrastructure/Jenkinsfile-CI.groovy
@@ -1,7 +1,7 @@
 pipeline {
       agent {
         dockerfile {
-            filename 'docker/dockerfile-java'
+            filename 'infrastructure/docker/Dockerfile'
             additionalBuildArgs  '--build-arg JENKINS_USER_ID=`id -u jenkins` --build-arg JENKINS_GROUP_ID=`id -g jenkins`'
         }
     }
@@ -9,7 +9,7 @@ pipeline {
     stages {
         stage('Test') {
             steps {
-                sh 'mvn clean test'
+                sh 'mvn -B -U clean test'
             }
         }
     }
