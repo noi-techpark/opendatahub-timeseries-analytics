47d5910e47a8559448629ffa16c4969bf956767e
Peter Moser <p.moser@noi.bz.it>
2020-06-10 15:09:00 +0200

    Add Keycloak config

--- src/main/webapp/config.js
+++ src/main/webapp/config.js
@@ -1,8 +1,8 @@
 var thunderforest_api_key = ''
 var open_mobility_api_uri = 'https://mobility.api.opendatahub.bz.it'
 
-var KEYCLOAK_AUTHORIZATION_URI = ''
-var KEYCLOAK_REALM = ''
-var KEYCLOAK_CLIENT_ID = ''
-var KEYCLOAK_REDIRECT_URI = ''
-var KEYCLOAK_SILENT_CHECK_SSO_REDIRECT_URI = ''
+var KEYCLOAK_AUTHORIZATION_URI = 'https://auth.opendatahub.testingmachine.eu/auth/realms/noi/protocol/openid-connect/auth'
+var KEYCLOAK_REALM = 'noi'
+var KEYCLOAK_CLIENT_ID = 'odh-mobility-analytics'
+var KEYCLOAK_REDIRECT_URI = 'http://localhost:8999/'
+var KEYCLOAK_SILENT_CHECK_SSO_REDIRECT_URI = 'http://localhost:8999/callback.html'
