f2672604f96782e06bead2655a943dc3605677de
Peter Moser <p.moser@noi.bz.it>
2020-06-10 15:32:20 +0200

    Add new dotenv.js instead of config.js
    This is to be consistent with other projects we have

---
+++ src/main/webapp/dotenv.js
@@ -0,0 +1,18 @@
+/**
+ * Update this file with the script infrastructure/dotenv-sed.sh
+ *
+ * 1) Copy .env.example to .env (place it into the projects root folder)
+ * 2) Execute "cd infrastructure && ./dotenv-sed.sh"
+ */
+window.env = {
+	SERVER_PORT: "8999",
+	ENDPOINT_URL: "https://analytics.opendatahub.testingmachine.eu",
+	ODH_MOBILITY_API_URI: "https://mobility.api.opendatahub.bz.it",
+	LOG_APPLICATION_NAME: "opendatahub-analytics",
+	THUNDERFOREST_MAPS_API_KEY: "",
+	KEYCLOAK_AUTHORIZATION_URI: "https://auth.opendatahub.testingmachine.eu/auth",
+	KEYCLOAK_REALM: "noi",
+	KEYCLOAK_CLIENT_ID: "odh-mobility-analytics",
+	KEYCLOAK_REDIRECT_URI: "https://analytics.opendatahub.testingmachine.eu/",
+	KEYCLOAK_SILENT_CHECK_SSO_REDIRECT_URI: "https://analytics.opendatahub.testingmachine.eu/callback.html",
+}
