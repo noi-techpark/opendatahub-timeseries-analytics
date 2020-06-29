9bb89540dbab2c715923ab1d05cd6124a9b2f29f
thomas <t.nocker.tn@gmail.com>
2020-06-29 16:43:11 +0200

    kepp old keycloak configuration + remove keycloak.json

--- src/main/webapp/dotenv.js
+++ src/main/webapp/dotenv.js
@@ -9,5 +9,10 @@ window.env = {
 	ENDPOINT_URL: "https://analytics.opendatahub.testingmachine.eu",
 	ODH_MOBILITY_API_URI: "https://mobility.api.opendatahub.testingmachine.eu",
 	LOG_APPLICATION_NAME: "opendatahub-analytics",
-	THUNDERFOREST_MAP_API_KEY: ""
+	THUNDERFOREST_MAP_API_KEY: "",
+	KEYCLOAK_AUTHORIZATION_URI: "https://auth.opendatahub.testingmachine.eu/auth",
+	KEYCLOAK_REALM: "noi",
+	KEYCLOAK_CLIENT_ID: "odh-mobility-analytics",
+	KEYCLOAK_REDIRECT_URI: "http://localhost:8999/",
+	KEYCLOAK_SILENT_CHECK_SSO_REDIRECT_URI: "http://localhost:8999/callback.html",
 }
